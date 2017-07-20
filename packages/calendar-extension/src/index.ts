99086fec67c0920bab6727e3dc254879ed2474c2
QuantLab <quantlab.io@gmail.com>
2017-07-20 18:14:38 +0800

    register calendar with .cal file

--- packages/calendar-extension/src/index.ts
+++ packages/calendar-extension/src/index.ts
@@ -6,11 +6,11 @@ import {
 } from '@quantlab/services';
 
 import {
-  QuantLab, QuantLabPlugin
+  ILayoutRestorer, QuantLab, QuantLabPlugin
 } from '@quantlab/application';
 
 import {
-  ICommandPalette, IMainMenu
+  InstanceTracker, ICommandPalette, IMainMenu
 } from '@quantlab/apputils';
 
 import {
@@ -22,7 +22,7 @@ import {
 } from '@phosphor/widgets';
 
 import {
-  Calendar
+  Calendar, CalendarFactory
 } from '@quantlab/calendar';
 
 import * as $
@@ -32,21 +32,23 @@ import 'fullcalendar';
 
 import 'fullcalendar/dist/gcal.js';
 
-import 'fullcalendar-scheduler/dist/scheduler.js';
-
 /**
  * The command IDs used by the calendar plugin.
  */
 namespace CommandIDs {
   export
-  const create = 'calendar:create';
+  const open = 'calendar:open';
 };
+
+/**
+ * The name of the factory that creates Calendar widgets.
+ */
+const FACTORY = 'Calendar';
 
 /**
  * The class name for the calendar icon in the default theme.
  */
 const CALENDAR_ICON_CLASS = 'jp-CalendarIcon';
-
 
 /**
  * The default calendar extension.
@@ -55,7 +57,7 @@ const plugin: QuantLabPlugin<void> = {
   activate,
   id: 'jupyter.extensions.calendar',
   requires: [
-    IServiceManager, IMainMenu, ICommandPalette
+    ILayoutRestorer, IServiceManager, IMainMenu, ICommandPalette
   ],
   optional: [ILauncher],
   autoStart: true
@@ -71,15 +73,39 @@ export default plugin;
 /**
  * Activate the calendar plugin.
  */
-function activate(app: QuantLab, services: IServiceManager, mainMenu: IMainMenu, palette: ICommandPalette, launcher: ILauncher | null): void {
+function activate(app: QuantLab, restorer: ILayoutRestorer, services: IServiceManager, mainMenu: IMainMenu, palette: ICommandPalette, launcher: ILauncher | null): void {
+  const factory = new CalendarFactory({
+    name: FACTORY,
+    fileExtensions: ['.cal'],
+    defaultFor: ['.cal'],
+    readOnly: true
+  });
+  const tracker = new InstanceTracker<Calendar>({ namespace: 'calendar' });
+
+  // Handle state restoration.
+  restorer.restore(tracker, {
+    command: 'docmanager:open',
+    args: widget => ({ path: widget.context.path, factory: FACTORY }),
+    name: widget => widget.context.path
+  });
+
+  app.docRegistry.addWidgetFactory(factory);
+  factory.widgetCreated.connect((sender, widget) => {
+    // Track the widget.
+    tracker.add(widget);
+    // Notify the instance tracker if restore data needs to update.
+    widget.context.pathChanged.connect(() => { tracker.save(widget); });
+  });
+
   const { commands, shell } = app;
   const category = 'Calendar';
 
-  commands.addCommand(CommandIDs.create, {
-    label: 'New Calendar',
+  commands.addCommand(CommandIDs.open, {
+    label: 'Open Calendar',
     execute: args => {
       let id = `jp-Calendar-${Private.id++}`;
-      let widget = new Calendar();
+
+      let widget = new Calendar({context:null});
       widget.id = id;
       widget.title.label = 'Calendar';
       widget.title.icon = 'CALENDAR_ICON_CLASS';
@@ -91,97 +117,20 @@ function activate(app: QuantLab, services: IServiceManager, mainMenu: IMainMenu,
       let calendar:JQuery = $('#' + id);
       calendar.fullCalendar({
         schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-  			editable: true,
-        //droppable: true,
   			aspectRatio: 2.0,
-  			//scrollTime: '00:00',
-        /*
         eventLimit: true,
         googleCalendarApiKey: 'AIzaSyDjh1p472rNVktbzltiO6NM7DRRNccx-t4',
         events: {
-          googleCalendarId: '41g8dii7l1mk3fqsr74ifl8o8s@group.calendar.google.com',
-          className: 'gcal-event'
+          googleCalendarId: '41g8dii7l1mk3fqsr74ifl8o8s@group.calendar.google.com'
         },
-        */
         header:{
-              left: 'addTask today prev,next',
+              left: 'today prev,next',
               center: 'title',
-              right: 'timelineDay,agendaWeek,month'
+              right: 'agendaDay,agendaWeek,month'
         },
-        customButtons: {
-  				addTask: {
-  					text: '+ task',
-  					click: function() {
-							calendar.fullCalendar(
-								'addResource',
-								{ title: 'new task' },
-								true // scroll to the new resource?
-							);
-  					}
-  				}
-  			},
-        defaultView: 'timelineDay',
-        resourceAreaWidth: '30%',
-        resourceColumns: [
-          {
-            labelText: 'Task',
-            field: 'title'
-          },
-          {
-            labelText: 'Priority',
-            field: 'priority'
-          }
-        ],
-        resources: [
-          { id: 'a', title: 'Task A', priority: 40 },
-          { id: 'b', title: 'Task B', priority: 40, eventColor: 'green' },
-          { id: 'c', title: 'Task C', priority: 40, eventColor: 'orange' },
-          { id: 'd', title: 'Task D', priority: 40, children: [
-            { id: 'd1', title: 'Task D1', priority: 10 },
-            { id: 'd2', title: 'Task D2', priority: 10 }
-          ] },
-          { id: 'e', title: 'Task E', priority: 40 },
-          { id: 'f', title: 'Task F', priority: 40, eventColor: 'red' },
-          { id: 'g', title: 'Task G', priority: 40 },
-          { id: 'h', title: 'Task H', priority: 40 },
-          { id: 'i', title: 'Task I', priority: 40 },
-          { id: 'j', title: 'Task J', priority: 40 },
-          { id: 'k', title: 'Task K', priority: 40 },
-          { id: 'l', title: 'Task L', priority: 40 },
-          { id: 'm', title: 'Task M', priority: 40 },
-          { id: 'n', title: 'Task N', priority: 40 },
-          { id: 'o', title: 'Task O', priority: 40 },
-          { id: 'p', title: 'Task P', priority: 40 },
-          { id: 'q', title: 'Task Q', priority: 40 },
-          { id: 'r', title: 'Task R', priority: 40 },
-          { id: 's', title: 'Task S', priority: 40 },
-          { id: 't', title: 'Task T', priority: 40 },
-          { id: 'u', title: 'Task U', priority: 40 },
-          { id: 'v', title: 'Task V', priority: 40 },
-          { id: 'w', title: 'Task W', priority: 40 },
-          { id: 'x', title: 'Task X', priority: 40 },
-          { id: 'y', title: 'Task Y', priority: 40 },
-          { id: 'z', title: 'Task Z', priority: 40 }
-        ],
-        events: [
-          { id: '1', resourceId: 'b', start: '2017-05-07T02:00:00', end: '2017-05-07T07:00:00', title: 'event 1' },
-          { id: '2', resourceId: 'c', start: '2017-05-07T05:00:00', end: '2017-05-07T22:00:00', title: 'event 2' },
-          { id: '3', resourceId: 'd', start: '2017-05-06', end: '2017-05-08', title: 'event 3' },
-          { id: '4', resourceId: 'e', start: '2017-05-07T03:00:00', end: '2017-05-07T08:00:00', title: 'event 4' },
-          { id: '5', resourceId: 'f', start: '2017-05-07T00:30:00', end: '2017-05-07T02:30:00', title: 'event 5' }
-        ]
+        defaultView: 'agendaWeek'
+      });
 
-      });
-      /*
-      calendar.on('resize', function(){
-        let ar = $('#' + id + '.fc-view').width() / $('#' + id + '.fc-view').height();
-        calendar.fullCalendar(
-          'option',
-          'aspectRatio',
-          ar
-        );
-      });
-      */
       return widget;
 
     }
@@ -191,7 +140,7 @@ function activate(app: QuantLab, services: IServiceManager, mainMenu: IMainMenu,
   let menu = new Menu({ commands });
   menu.title.label = category;
   [
-    CommandIDs.create
+    CommandIDs.open
   ].forEach(command => {
     palette.addItem({ command, category });
     menu.addItem({ command });
@@ -206,16 +155,19 @@ function activate(app: QuantLab, services: IServiceManager, mainMenu: IMainMenu,
       rank: 1,
       iconClass: CALENDAR_ICON_CLASS,
       callback: () => {
-        return commands.execute(CommandIDs.create);
+        return commands.execute(CommandIDs.open);
       }
     });
   }
+
+
 }
 
 /**
  * A namespace for private data.
  */
 namespace Private {
+
   /**
    * An incrementing counter for ids.
    */
--- packages/calendar/src/widget.ts
+++ packages/calendar/src/widget.ts
@@ -2,8 +2,33 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
+  PromiseDelegate
+} from '@phosphor/coreutils';
+
+import {
+  Message
+} from '@phosphor/messaging';
+
+import {
   Widget
 } from '@phosphor/widgets';
+
+import {
+  ActivityMonitor
+} from '@quantlab/coreutils';
+
+import {
+  ABCWidgetFactory, DocumentRegistry
+} from '@quantlab/docregistry';
+
+import * as $
+  from 'jquery';
+
+import 'fullcalendar';
+
+import 'fullcalendar/dist/gcal.js';
+
+import 'fullcalendar-scheduler/dist/scheduler.js';
 
 /**
  * The class name added to a calendar widget.
@@ -11,28 +36,119 @@ import {
 const CALENDAR_CLASS = 'jp-Calendar';
 
 /**
+ * The timeout to wait for change activity to have ceased before rendering.
+ */
+const RENDER_TIMEOUT = 1000;
+
+/**
  * A widget which manages a calendar session.
  */
 export
-class Calendar extends Widget {
+class Calendar extends Widget implements DocumentRegistry.IReadyWidget {
   /**
    * Construct a new calendar widget.
    *
    * @param options - The calendar configuration options.
    */
-  constructor(options: Calendar.IOptions = {}) {
+  constructor(options: Calendar.IOptions) {
     super();
+
     this.addClass(CALENDAR_CLASS);
 
+    let context = this._context = options.context;
+
+    if(context){
+      this.title.label = context.path.split('/').pop();
+      context.pathChanged.connect(this._onPathChanged, this);
+
+      this._context.ready.then(() => {
+        this._updateCalendar();
+        this._ready.resolve(undefined);
+        // Throttle the rendering rate of the widget.
+        this._monitor = new ActivityMonitor({
+          signal: context.model.contentChanged,
+          timeout: RENDER_TIMEOUT
+        });
+        this._monitor.activityStopped.connect(this._updateCalendar, this);
+      });
+    }else{
+      this.title.label = 'Calendar';
+    }
+
+  }
+
+  /**
+   * The Calendar widget's context.
+   */
+  get context(): DocumentRegistry.Context {
+    return this._context;
+  }
+
+  /**
+   * A promise that resolves when the calendar is ready.
+   */
+  get ready() {
+    return this._ready.promise;
   }
 
   /**
    * Dispose of the resources held by the calendar widget.
    */
   dispose(): void {
+    let monitor = this._monitor;
+    this._monitor = null;
+    if (monitor) {
+      monitor.dispose();
+    }
     super.dispose();
   }
 
+  /**
+   * Handle `'activate-request'` messages.
+   */
+  protected onActivateRequest(msg: Message): void {
+    this.node.tabIndex = -1;
+    this.node.focus();
+  }
+
+  /**
+   * Handle a change in path.
+   */
+  private _onPathChanged(): void {
+    this.title.label = this._context.path.split('/').pop();
+  }
+
+  /**
+   * Create the json model for the calendar.
+   */
+  private _updateCalendar(): void {
+    let content = this._context.model.toString();
+    console.log(content);
+    this._calendar = $('#' + this.id);
+    this._calendar.fullCalendar({
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+      editable: true,
+      //droppable: true,
+      aspectRatio: 2.0,
+      //scrollTime: '00:00',
+      eventLimit: true,
+      header:{
+            left: 'today prev,next',
+            center: 'title',
+            right: 'timelineDay,agendaWeek,month'
+      },
+      defaultView: 'timelineDay',
+      resourceAreaWidth: '30%',
+      resourceColumns: JSON.parse(content).resourceColumns,
+      resources: JSON.parse(content).resources,
+      events: JSON.parse(content).events
+    });
+  }
+
+  private _context: DocumentRegistry.Context = null;
+  private _ready = new PromiseDelegate<void>();
+  private _monitor: ActivityMonitor<any, any> = null;
+  private _calendar: JQuery = null;
 }
 
 /**
@@ -45,6 +161,22 @@ namespace Calendar {
    */
   export
   interface IOptions {
+    /**
+     * The document context for the Calendar being rendered by the widget.
+     */
+    context: DocumentRegistry.Context;
+  }
+}
 
+/**
+ * A widget factory for Calendar widgets.
+ */
+export
+class CalendarFactory extends ABCWidgetFactory<Calendar, DocumentRegistry.IModel> {
+  /**
+   * Create a new widget given a context.
+   */
+  protected createNewWidget(context: DocumentRegistry.Context): Calendar {
+    return new Calendar({ context });
   }
 }
